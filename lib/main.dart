eb3e7f0ecb372985800bd3ce488665b47937b6f2
coiniseasy <coiniseasy@gmail.com>
2023-02-03 23:59:37 +0900

    Webtoon

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
-import 'package:coineasy/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:jaden/screens/home_screen.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
-class App extends StatefulWidget {
-  @override
-  State createState() => _AppState();
-}
+class App extends StatelessWidget {
+  const App({super.key});
 
-class _AppState extends State {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: const Color(0xFFE7626C),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
